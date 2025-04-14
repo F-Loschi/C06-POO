0e780307756d77a3f1e60ec1a4d703a614e35351
Felipe Silva Loschi <166191574+F-Loschi@users.noreply.github.com>
2025-04-14 18:34:40 -0300

    Add files via upload

---
+++ POO - Aula 14 - Classe Abstrata/Mamiferos/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Cachorro C1 = new Cachorro("Cleber", 600);
+        Lontra L1 = new Lontra("Robertinha", 8000);
+        Boi B1 = new Boi("Creison", 500);
+
+        B1.emitirSom();
+        B1.mostraInfo();
+
+        C1.emitirSom();
+        C1.mostraInfo();
+
+        L1.emitirSom();
+        L1.mostraInfo();
+    }
+}
