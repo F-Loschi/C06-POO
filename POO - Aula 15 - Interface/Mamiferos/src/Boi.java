ea778f1fe950d9acb16c19411c7141f23c2b07b4
Felipe Silva Loschi <166191574+F-Loschi@users.noreply.github.com>
2025-04-15 14:20:10 -0300

    Mamíferos com Interface para a lontra

---
+++ POO - Aula 15 - Interface/Mamiferos/src/Boi.java
@@ -0,0 +1,10 @@
+public class Boi extends Mamifero{
+    public Boi(String nome, double vida) {
+        super(nome, vida);
+    }
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Bitch I'm a cow, bitch I'm a cow, I'm not a cat, I don't say meow!");
+    }
+}
